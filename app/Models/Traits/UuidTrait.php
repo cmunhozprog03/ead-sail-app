1f60d11813dfc6019b4d6174974d997e559c90b2
cmunhozprog03 <cmunhozprog03@hotmail.com>
2022-10-26 20:25:25 -0300

    correction uuid

--- app/Models/Traits/UuidTrait.php
+++ app/Models/Traits/UuidTrait.php
@@ -4,6 +4,9 @@ namespace App\Models\Traits;
 use Illuminate\Support\Str;
 
 trait UuidTrait {
+
+  
+
   public static function booted(){
     static::creating(function($model){
         $model->{$model->getKeyName()} = (string) Str::uuid();
